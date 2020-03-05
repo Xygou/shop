ab57689ac4fe98b948bb1f8cb9d7e3cfc49e69e0
Your Name <you@example.com>
2020-03-05 22:14:41 +0200

    first commit

--- test_project/urls.py
+++ test_project/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 admin.autodiscover()
 
@@ -23,4 +25,6 @@ urlpatterns = [
     path('', include('shop.urls')),
     path('', include('products.urls')),
     path('', include('orders.urls'))
-]
+] \
+              + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
+              + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
